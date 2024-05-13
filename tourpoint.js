06e138523f59809e3f997b44c3ebb662e7d96703
naruebetruangdet <naruebet.ruangdet@g.swu.ac.th>
2024-05-13 16:48:56 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,156 @@
+
+var tourpoint={
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ม่อนแจ่มเชียงใหม่",
+        "type": "natural",
+        "level": 5
+      },
+      "geometry": {
+        "coordinates": [
+          98.82268527116577,
+          18.93559124807031
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ภูกระดึง เลย",
+        "type": "natural",
+        "level": 4
+      },
+      "geometry": {
+        "coordinates": [
+          101.87193354232869,
+          16.881105177189625
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ICONSIAM",
+        "type": "special",
+        "level": 5
+      },
+      "geometry": {
+        "coordinates": [
+          100.51043649999775,
+          13.726760138098655
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เกาะเสม็ด",
+        "type": "natural",
+        "level": 5
+      },
+      "geometry": {
+        "coordinates": [
+          101.44847518650738,
+          12.539688927971511
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "วัดเซ็นโซจิ",
+        "type": "cultural",
+        "level": 5
+      },
+      "geometry": {
+        "coordinates": [
+          139.79650106151018,
+          35.71217383343233
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "วัดใหญ่ชัยมงคล",
+        "type": "cultural",
+        "level": 4
+      },
+      "geometry": {
+        "coordinates": [
+          100.59256127116856,
+          14.345840218313526
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ภูเก็จเพลินวาน",
+        "type": "natural",
+        "level": 3
+      },
+      "geometry": {
+        "coordinates": [
+          98.39221632216527,
+          7.880489199721623
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ปัตตานี",
+        "type": "special",
+        "level": 3
+      },
+      "geometry": {
+        "coordinates": [
+          101.19844352452873,
+          6.8509960234194125
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "สิงคโปร์ฟลายเออร์",
+        "type": "natural",
+        "level": 4
+      },
+      "geometry": {
+        "coordinates": [
+          103.86329999015408,
+          1.2895933401710096
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Eiffel Tower",
+        "type": "special",
+        "level": 5
+      },
+      "geometry": {
+        "coordinates": [
+          2.2944748637544023,
+          48.858311521788664
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
